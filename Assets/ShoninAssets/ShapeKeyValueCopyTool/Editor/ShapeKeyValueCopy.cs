8120399824cce84f130f74a0faf016a67378baf1
ShoninVRC <56765138+ShoninVRC@users.noreply.github.com>
2024-03-01 03:29:11 +0900

    ShapeKeyValueCopyを追加
    Copy meshes root及びPaste meshes rootにそれぞれオブジェクトを割り当ててApplyボタンを押すと、Copy meshes root以下の全ての子オブジェクトにアタッチされているSkinnedMeshRendererのシェイプキーの値を、Paste meshes root以下の名前が同じ子オブジェクトにアタッチされているSkinnedMeshRendererの同名シェイプキーの値にコピーします。

---
+++ Assets/ShoninAssets/ShapeKeyValueCopyTool/Editor/ShapeKeyValueCopy.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[Serializable]
+public class ShapeKeyValueCopy : EditorWindow
+{
+    public static Transform copyTransform;
+    public static Transform pasteTransform;
+
+    [MenuItem("ShoninAssets/Shape Key Value Copy")]
+    public static void Create()
+    {
+        var window = GetWindow<ShapeKeyValueCopy>("Shape Key Value Copy");
+        window.minSize = new Vector2(300, 60);
+    }
+
+    private void OnGUI()
+    {
+        copyTransform = EditorGUILayout.ObjectField("Copy meshes root", copyTransform, typeof(Transform)) as Transform;
+        pasteTransform = EditorGUILayout.ObjectField("Paste meshes root", pasteTransform, typeof(Transform)) as Transform;
+
+        if (copyTransform && pasteTransform)
+        {
+            if (GUILayout.Button("Apply"))
+            {
+                List<SkinnedMeshRenderer> copyRenderers = new List<SkinnedMeshRenderer>();
+                GetChildrenRenderers(copyTransform, ref copyRenderers);
+                List<SkinnedMeshRenderer> pasteRenderers = new List<SkinnedMeshRenderer>();
+                GetChildrenRenderers(pasteTransform, ref pasteRenderers);
+
+                foreach (SkinnedMeshRenderer copyRenderer in copyRenderers)
+                {
+                    foreach (SkinnedMeshRenderer pasteRenderer in pasteRenderers)
+                    {
+                        if (pasteRenderer.transform.name == copyRenderer.transform.name)
+                        {
+                            Undo.RecordObject(pasteRenderer, "Copy Shape Key Values");
+                            int blendShapeCount = copyRenderer.sharedMesh.blendShapeCount;
+                            for (int i = 0; i < blendShapeCount; i++)
+                            {
+                                pasteRenderer.SetBlendShapeWeight(i, copyRenderer.GetBlendShapeWeight(i));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public static void GetChildrenRenderers(Transform obj, ref List<SkinnedMeshRenderer> allChildren)
+    {
+        Transform children = obj.GetComponentInChildren<Transform>();
+        if (children.childCount == 0)
+        {
+            return;
+        }
+        foreach (Transform ob in children)
+        {
+            SkinnedMeshRenderer renderer = ob.GetComponent<SkinnedMeshRenderer>();
+            if (renderer)
+            {
+                allChildren.Add(renderer);
+            }
+            GetChildrenRenderers(ob, ref allChildren);
+        }
+    }
+
+}
